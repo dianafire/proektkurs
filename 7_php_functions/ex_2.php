3e837f6ab2036d96f41987d8d009d94d7d353d75
dianafire <diana_fire@abv.bg>
2015-05-18 07:59:28 -0700

    adding a reference for var number in ex.2

--- 7_php_functions/ex_2.php
+++ 7_php_functions/ex_2.php
@@ -41,7 +41,7 @@
 
 	}
 
-
+	echo "WITHOUT REFERENCE FOR NUMBER - NO CHANGE:<br />";
 
 	$strArray=array("boy","girl","момче","момиче","господин","госпожица");
 
@@ -50,6 +50,46 @@
 		numString($word);
 	}
 
+
+
+	echo "<br /> <br />";
+
+
+	function numStringRef($word,&$number){
+
+		$lenght=mb_strlen($word,'UTF-8');
+
+		echo "Number at the beginning: $number, ";
+
+		if ($lenght%2===0){
+			$number+=12;
+		}
+
+		else{
+			$number-=3;
+		}
+
+		if ($number>10){
+			$number-=22;
+		}
+
+		else if($number<0){
+			$number+=14;
+		}
+
+		echo "word: $word, lenght: $lenght, number at the end: $number; <br />";
+
+	}
+
+	
+	echo "USING REFERENCE FOR NUMBER - CHANGE EVERY TIME:<br />";
+
+	$number=6;
+
+	foreach ($strArray as $word){          // call the function 6 times with different word and different number (because of the reference)
+
+		numStringRef($word,$number);
+	}
 
 
 ?>
